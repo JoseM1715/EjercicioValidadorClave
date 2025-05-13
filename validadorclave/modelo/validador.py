af2db444108c1f96cc890c16e0cd3c67352ff323
JoseAngel <martinez1215sanchez@gmail.com>
2025-05-12 23:32:31 -0500

    La clase ReglaValidacion Facilita la construcion de reglas flexibles y reutilizables para validar contraseñas de forma sólida. Este abstracto contempla validacion estandar como longitud.

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,27 @@
+from  abc import ABC, abstractmethod
+
+
+
+class ReglaValidacion(ABC):
+    def __init__(self, _longitud_esperada: int) -> None:
+        self._longitud_espera: int = _longitud_esperada
+
+    def _validar_longitud(self, clave: str)-> bool:
+        return len(clave) > self._longitud_espera
+
+    def _contiene_mayuscula(self, clave: str)->bool:
+        return any(char.isupper()for char in clave)
+
+    def _contiene_minuscula(sel, clave: strc)-> bool:
+            return any(char.isdigit()for char in clave )
+
+    def _contiene_numero(sel, clave: strc)-> bool:
+            return any(char.isdigit()for char in clave )
+
+
+
+
+
+
+
 # TODO: Implementa el código del ejercicio aquí
